2a76404cd65b051aed1dac5c1519700cc9f3a5f6
pruje839 <jeffprumbaum@hotmail.com>
2025-03-27 15:38:45 +0100

    var durch let ersetzt

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /*
 Skript für Neuseelandreise
-
 */
 
 //Karte initialisieren
-var map = L.map('map').setView([-44.616667, 167.866667], 12);
+let map = L.map('map').setView([-44.616667, 167.866667], 12);
+
 //Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -12,7 +12,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //Marker zeichnen 
-var marker = L.marker([-44.616667, 167.866667]).addTo(map);
+let marker = L.marker([-44.616667, 167.866667]).addTo(map);
 
 //Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
