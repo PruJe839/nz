c391123570fe815d6946c26ccb7eb6968fd99510
pruje839 <jeffprumbaum@hotmail.com>
2025-03-27 15:36:43 +0100

    Kommentare ergenzt

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+/*
+Skript für Neuseelandreise
+
+*/
+
+//Karte initialisieren
 var map = L.map('map').setView([-44.616667, 167.866667], 12);
+//Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+//Marker zeichnen 
 var marker = L.marker([-44.616667, 167.866667]).addTo(map);
+
+//Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
